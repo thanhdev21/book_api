4d548cbdaf167e6260ca24146e5aae35b32b5777
kiennguyen-alobridge <95597958+kiennguyen-alobridge@users.noreply.github.com>
2022-03-23 17:08:32 +0700

    task #6635 Write the new book list Api (#37)

--- src/services/books/queries/index.ts
+++ src/services/books/queries/index.ts
@@ -2,9 +2,11 @@ import { QueryResolvers } from '@graphql/types/generated-graphql-types';
 import { getAllBooks } from './getAllBooks';
 import { getBook } from './getBook';
 import { getBookByRank } from './getBookByRank';
+import { getNewBookList } from './getNewBookList';
 
 export const bookQueries: QueryResolvers = {
   getAllBooks,
   getBook,
   getBookByRank,
+  getNewBookList,
 };
